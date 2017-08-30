b716db04edcfb677ceb83531b050dbf23b6e82ac
Robert Pofuk <rpofuk@gmail.com>
2017-08-30 16:24:06 +0200

    Initial commit

---
+++ src/main/java/com/loraneo/test/infinispan/InfinispanTest.java
@@ -0,0 +1,41 @@
+package com.loraneo.test.infinispan;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.infinispan.Cache;
+
+@ApplicationScoped
+@Path("/cache")
+public class InfinispanTest {
+
+    @Inject
+    Cache<String, String> cache;
+
+    @GET
+    @Path("get")
+    @Produces("text/plain")
+    public Response getMessage() {
+        return Response.ok(cache.keySet()
+                .toString())
+                .build();
+    }
+
+    @GET
+    @Path("put/{key}/{data}")
+    @Produces("text/plain")
+    public Response putMessage(@PathParam("key") final String key,
+                               @PathParam("data") final String data) {
+        cache.put(key,
+                data);
+        return Response.created(null)
+                .build();
+
+    }
+
+}
